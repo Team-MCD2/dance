78d60ac642363af493664df057bbf301ac281dbe
brad <mbosseubradbruel@gmail.com>
2026-05-18 10:54:10 +0200

    feat: make project deployable on Vercel; ignore Python scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: '/albums', destination: '/pages/albums.html' },
+      { source: '/planning-tarifs', destination: '/pages/planning-tarifs.html' },
+      { source: '/livre-d-or', destination: '/pages/livre-d-or.html' },
+      { source: '/faq', destination: '/pages/faq.html' },
+      { source: '/mentions-legales', destination: '/pages/mentions-legales.html' },
+      { source: '/depot-dun-temoignage', destination: '/pages/depot-dun-temoignage.html' },
+    ];
+  },
 };
 
 export default nextConfig;
